737f681a2ff8637b8931dbf94ef1d68ff06f4099
B17_Shishani <83888130+MoadBino@users.noreply.github.com>
2022-09-26 00:43:08 +0300

    start client side

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,10 @@
 const express = require("express");
+const cors = require("cors");
 const connection = require("./model/db");
-
 const catRouter = require("./routes/cat");
-
 const app = express();
+app.use(cors());
 app.use(express.json())
-
 app.use("/cat", catRouter);
 
 const PORT = 5000;
